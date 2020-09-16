640d884609a789e3e167abea0c31acbf7c2b46df
AnamikaGautam52 <annu436gautam@gmail.com>
2020-09-16 10:57:32 +0000

    person added

---
+++ src/main/java/com/FirstWebApp/model/Person.java
@@ -0,0 +1,65 @@
+package com.FirstWebApp.model;
+
+import java.util.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "collections")
+public class Person {
+  @Id
+  private String id;
+
+  private String firstname;
+  private String lastname;
+  private String gender;
+  private Date dob;
+
+  public Person() {
+
+  }
+ public Person(String firstname, String lastname, String gender, Date dob) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.gender = gender;
+      this.dob = dob;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+ public String getGender() {
+    return gender;
+  }
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public Date getDob() {
+    return dob;
+  }
+
+  public void setDob(Date dob) {
+    this.dob = dob;
+  }
+
+  @Override
+  public String toString() {
+    return "Person [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", gender=" + gender + ", dob=" + dob + "]";
+  }
+}
